1a303b5b1c5eb362d1925b2f2075487ca7169ba7
Ruimas-UTI <allpraisethelord@gmail.com>
2021-06-25 18:03:11 +0600

    navbar fixed

--- src/components/navbar/navbar.component.js
+++ src/components/navbar/navbar.component.js
@@ -1,14 +1,21 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import {Navbar,Nav,Container,Button} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 export default function NavigationBar() {
     const [windowWidth, setWindowWidth] = useState(window.innerWidth)
-    return (
+    const handleWidthChange = () => {
+        setWindowWidth(window.innerWidth)
+    }
+    useEffect(() =>{
+        window.addEventListener('resize',handleWidthChange)
+    },[windowWidth])
+    if(windowWidth > 991){
+        return (
             <Navbar bg="light" expand="lg">
                <Container>
                     <Navbar.Brand href="#" ><img alt='' src='tick.png'/></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse className="justify-content-between">
+                        <Navbar.Collapse className="justify-content-between mr-auto">
                             <Nav className="justify-content-start">
                                 <Nav.Link href="#">More...</Nav.Link>
                                 <Nav.Link href="#">Services</Nav.Link>
@@ -24,5 +31,30 @@ export default function NavigationBar() {
                         </Button>
                </Container>
             </Navbar>
+        )
+    } else {
+        return (
+            <Navbar collapseOnSelect expand="lg" bg="light">
+                <Navbar.Brand href="#" ><img alt='' src='tick.png'/></Navbar.Brand>
+                <Nav className='justify-content-center'>
+                    <Button variant="outline-secondary">
+                                <a alt='' href="/">Sign in</a> / <a href='/'>Sign Up</a>
+                    </Button>
+                </Nav>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/">Features</Nav.Link>
+                        <Nav.Link href="/">Pricing</Nav.Link>
+                        </Nav>
+                        <Nav>
+                        <Nav.Link href="/">More deets</Nav.Link>
+                        <Nav.Link eventKey={2} href="/">
+                            Dank memes
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
     )
+    }
 }
